7ad852f0509170d2ce92a09ce435674db3386e1b
zugzpark <tjdwls9355@gmail.com>
2022-02-22 21:47:16 +0900

    Day1

---
+++ javascript/CodingTest/LEVEL_1/신고 결과 받기.js
@@ -0,0 +1,34 @@
+function solution(id_list, report, k) {
+
+    let reportObj = new Map();
+
+    //* 테크니션 연습
+    id_list.forEach(val => {
+        (reportObj[val] = {
+            report: [],
+            reported: 0,
+            isStoppedId: false,
+        });
+    });
+
+    report.forEach(val => {
+        let [reportPerson, reportedPerson] = val.split(' ');
+
+        if (reportObj[reportPerson].report.indexOf(reportedPerson) === -1) {
+            reportObj[reportPerson].report.push(reportedPerson);
+            reportObj[reportedPerson].reported += 1;
+
+            if (reportObj[reportedPerson].reported >= k) {
+                reportObj[reportedPerson].isStoppedId = true;
+            }
+        }
+    });
+
+    let answer = id_list.map(val => {
+        return reportObj[val].report.filter((reportId) => reportObj[reportId].isStoppedId).length;
+    });
+
+
+    
+    return answer;
+}
---
+++ javascript/CodingTest/LEVEL_1/약수의 개수와 덧셈.js
@@ -0,0 +1,25 @@
+function solution(left, right) {
+    var answer = 0;
+    let count = 0;
+
+    for(var i=left ; i<=right ; i++){
+        for(var j=1 ; j<=i ; j++){
+            if(i%j==0){
+                count++
+            } 
+            if(i==j){
+                if(count%2==0){
+                    answer+=i    
+                }else{
+                    answer-=i;
+                }
+                count = 0 ;
+            }
+        }
+    }
+    return answer;
+
+    //제곱근이 정수면 약수의 갯수가 홀수를 활용
+    // if (Number.isInteger(Math.sqrt(i))) << 명답
+ 
+}
