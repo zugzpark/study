6b5ae06b7feb89c8c5eddce7159d149d8d0224e5
zugzpark <tjdwls9355@gmail.com>
2022-02-23 22:31:00 +0900

    Day2

---
+++ javascript/CodingTest/LEVEL_1/내적.js
@@ -0,0 +1,6 @@
+function solution(a, b) {
+
+    //reduce() 로 간단히 표현 ,0 << index의 시작점 설정
+    return a.reduce((acc, val, i) => acc += val * b[i], 0)
+
+}
---
+++ javascript/CodingTest/LEVEL_1/약수의 합.js
@@ -0,0 +1,13 @@
+function solution(n) {
+
+    answer = 0;
+
+    for (let i = 0; i <= n; i++) {
+        if (n % i == 0) {
+            answer += i;
+        }
+    }
+
+    //삼항연산자로 푼 해답 return n<=a/2?b:solution(n,a+1,b+=n%a?0:a); 
+    return answer;
+}
