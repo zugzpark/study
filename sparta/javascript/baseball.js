36cb4c0b1d20ea0adc8d437712148752783317ed
박성진 <tjdwls9355@gmail>
2023-12-12 13:53:56 +0900

    chore : 조건문 가독성 수정

--- sparta/javascript/baseball.js
+++ sparta/javascript/baseball.js
@@ -13,7 +13,7 @@ let gameStart = true;
 function baseBall() {
 
     let message = '컴퓨터가 숫자를 생성하였습니다. 답을 맞춰보세요!';
-    
+
     if (!gameStart) {
         message = '서로 다른 숫자 3개를 입력해주세요!';
         gameNum--;
@@ -27,14 +27,19 @@ function baseBall() {
 
 function challenge() {
     rl.question(`${gameNum}번째 시도 : `, (answer) => {
-        let [a,b,c] = answer.split('');
-        
-        if (answer.length !== 3 || isNaN(answer * 1) || a==b || b==c || a==c) {
+        let [num1, num2, num3] = answer.split('');
+
+        if (answer.length !== 3 ||
+            isNaN(Number(answer)) ||
+            num1 == num2 ||
+            num2 == num3 ||
+            num3 == num1) {
+                
             gameNum++
             gameStart = false;
             return baseBall();
         } else {
-            
+
         }
 
         if (answer == computerNum) {
@@ -42,23 +47,24 @@ function challenge() {
             console.log(`3S\n${gameNum}번만에 맞히셨습니다.\n게임을 종료합니다.`);
 
         } else {
-            
+
             let ball = 0;
             let strike = 0;
-            
-                for(let i = 0 ; i<computerNum.length ; i++){
-                
-                if(computerNum[i]==answer[i]){
+
+            for (let i = 0; i < computerNum.length; i++) {
+
+                if (computerNum[i] == answer[i]) {
+
                     strike++;
-                    
+
                 } else {
-                    
-                    if(computerNum.includes(answer[i]))ball++;
+
+                    if (computerNum.includes(answer[i])) ball++;
                 }
             }
 
-            ball==3?console.log(`${ball}B`):console.log(`${ball}B ${strike}S`);
-                        
+            ball == 3 ? console.log(`${ball}B`) : console.log(`${ball}B ${strike}S`);
+
             gameNum++
 
             return challenge();
