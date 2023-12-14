d16d22c401f68390914bbff87fdcf9f85895b169
Seong Jin Park <70117647+zugzpark@users.noreply.github.com>
2023-12-14 11:33:39 +0900

    Update baseball.js

--- sparta/javascript/baseball.js
+++ sparta/javascript/baseball.js
@@ -28,46 +28,46 @@ function baseBall() {
 function challenge() {
     rl.question(`${gameNum}번째 시도 : `, (answer) => {
         let [num1, num2, num3] = answer.split('');
-
+        
         if (answer.length !== 3 ||
             isNaN(Number(answer)) ||
             num1 == num2 ||
             num2 == num3 ||
             num3 == num1) {
-                
+
             gameNum++
             gameStart = false;
             return baseBall();
-        } else {
-
-        }
-
-        if (answer == computerNum) {
-
-            console.log(`3S\n${gameNum}번만에 맞히셨습니다.\n게임을 종료합니다.`);
 
         } else {
 
-            let ball = 0;
-            let strike = 0;
+            if (answer == computerNum) {
 
-            for (let i = 0; i < computerNum.length; i++) {
+                console.log(`3S\n${gameNum}번만에 맞히셨습니다.\n게임을 종료합니다.`);
 
-                if (computerNum[i] == answer[i]) {
+            } else {
 
-                    strike++;
+                let ball = 0;
+                let strike = 0;
 
-                } else {
+                for (let i = 0; i < computerNum.length; i++) {
 
-                    if (computerNum.includes(answer[i])) ball++;
+                    if (computerNum[i] == answer[i]) {
+
+                        strike++;
+
+                    } else {
+
+                        if (computerNum.includes(answer[i])) ball++;
+                    }
                 }
+
+                ball == 3 ? console.log(`${ball}B`) : console.log(`${ball}B ${strike}S`);
+
+                gameNum++
+
+                return challenge();
             }
-
-            ball == 3 ? console.log(`${ball}B`) : console.log(`${ball}B ${strike}S`);
-
-            gameNum++
-
-            return challenge();
         }
         rl.close();
     });
