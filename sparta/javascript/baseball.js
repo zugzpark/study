3552c29d1d4010f0eec60a4cc515ca9dffce375e
Seong Jin Park <70117647+zugzpark@users.noreply.github.com>
2023-12-14 12:08:55 +0900

    Update baseball.js

--- sparta/javascript/baseball.js
+++ sparta/javascript/baseball.js
@@ -10,13 +10,15 @@ let computerNum = randomNum();
 let gameStart = true;
 
 
+//게임 실행 함수
 function baseBall() {
 
     let message = '컴퓨터가 숫자를 생성하였습니다. 답을 맞춰보세요!';
 
+    //길이가 2이하거나 중복숫자일 경우
     if (!gameStart) {
         message = '서로 다른 숫자 3개를 입력해주세요!';
-        gameNum--;
+
     }
 
     console.log(message);
@@ -25,29 +27,27 @@ function baseBall() {
 
 }
 
+//게임 시도 함수
 function challenge() {
     rl.question(`${gameNum}번째 시도 : `, (answer) => {
-        let [num1, num2, num3] = answer.split('');
 
-        
-        if (answer.length !== 3 ||
-            isNaN(Number(answer)) ||
-            num1 == num2 ||
-            num2 == num3 ||
-            num3 == num1) {
 
-            gameNum++
+        //중복, 길이가 3인지 여부 확인
+        if ((new Set(answer)).size !== answer.length || answer.length !== 3) {
+
             gameStart = false;
             return baseBall();
 
         } else {
 
+            //입력과 랜덤숫자가 같을때
             if (answer == computerNum) {
 
                 console.log(`3S\n${gameNum}번만에 맞히셨습니다.\n게임을 종료합니다.`);
 
-            } else {
 
+            } else {
+                //볼과 스트라이크 판별
                 let ball = 0;
                 let strike = 0;
 
@@ -57,9 +57,9 @@ function challenge() {
 
                         strike++;
 
-                    } else {
+                    } else if (computerNum.includes(answer[i])) {
 
-                        if (computerNum.includes(answer[i])) ball++;
+                        ball++;
                     }
                 }
 
@@ -75,7 +75,7 @@ function challenge() {
 }
 
 
-
+//랜덤 숫자 생성
 function randomNum() {
     let result = '';
 
@@ -92,5 +92,5 @@ function randomNum() {
     return result;
 }
 
-
+//게임 실행
 baseBall()
