13eb0fc3cbdb4a785f778fe3498835238108702e
박성진 <tjdwls9355@gmail>
2023-12-28 13:22:21 +0900

    feat : test

---
+++ sparta/test/schemas/member.schema.js
@@ -0,0 +1,21 @@
+import mongoose, { isObjectIdOrHexString } from "mongoose";
+
+const memberSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,requied:true,unique:true
+  },
+  pw: {
+    type: String,
+  },
+    
+},{versionKey:false});
+
+memberSchema.virtual("userId").get(function () {
+  return this._id.toHexString();
+});
+memberSchema.set('toJSON',{virtuals:true})
+
+export default mongoose.model("member", memberSchema);
