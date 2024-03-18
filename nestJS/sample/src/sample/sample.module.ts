3dc106ce43c648fb4ebc8df26111d53f62a9fb4b
박성진 <tjdwls9355@gmail>
2024-03-18 11:41:41 +0900

    fix : sample example

--- nestJS/sample/src/sample/sample.module.ts
+++ nestJS/sample/src/sample/sample.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SampleService } from './sample.service';
 import { SampleController } from './sample.controller';
+import { SampleBestService } from './sample.serviceBestService';
 
 @Module({
   controllers: [SampleController],
-  providers: [SampleService],
+  providers: [{
+    provide: 'SampleService',
+    useClass: SampleBestService
+  }],
 })
 export class SampleModule {}
